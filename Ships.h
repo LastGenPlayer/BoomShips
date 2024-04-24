c77aa2378ebc86c13da4b7ae86c70da53179c6fb
rainverrev <rainverrev@gmail.com>
2024-04-24 15:32:23 +0300

    yesh

--- Ships.h
+++ Ships.h
@@ -5,9 +5,24 @@
 #ifndef BOOMSHIPS_SHIPS_H
 #define BOOMSHIPS_SHIPS_H
 
+struct Coord {
+    int x, y;
+    bool isHit;
+};
 
 class Ships {
-
+    int shipLength;
+    Coord shipCoords[];
+public:
+    explicit Ships(int x, int y, int ship_length, bool is_horizontal)
+        : shipLength(ship_length) {
+        Coord shipCoords[shipLength];
+        if (is_horizontal) {
+            for (int i = 0; i < ship_length; i++) {
+                shipCoords[i] = Coord(x + i, y, false);
+            }
+        }
+    }
 };
 
 
