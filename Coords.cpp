a78e56e8c064fedf975546a032f06191ec6796de
rainverrev <rainverrev@gmail.com>
2024-04-27 16:23:39 +0300

    yesh5

---
+++ Coords.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Rain Verrev on 27.04.2024.
+//
+
+#include <iostream>
+
+class Coord {
+public:
+    int x, y;
+    bool isHit, isShip;
+
+    Coord() = default;
+
+    Coord(int x, int y, bool isShip)
+        : x(x),
+          y(y),
+          isHit(false),
+          isShip(isShip){
+    }
+
+    static bool isNeighbour(Coord P1, Coord P2) {
+        return std::max(std::abs(P1.x - P2.x), std::abs(P1.y - P2.y)) <= 1;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Coord &coord) {
+        os << "x: " << coord.x << " y: " << coord.y << " isHit: " << coord.isHit << " isShip: " << coord.isShip;
+        return os;
+    }
+};
